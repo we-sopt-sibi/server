4c27cebea081da901d83a726d632ec6e997e5190
gunom <gu3062@naver.com>
2021-11-25 15:00:12 +0900

    [update]: article GET comments data 추가

--- functions/db/article.js
+++ functions/db/article.js
@@ -1,6 +1,6 @@
 const _ = require('lodash');
 const convertSnakeToCamel = require('../lib/converSnakeToCamel');
-
+const dayjs = require('dayjs')
 const getArticleById = async (client, articleId) => {
     const {rows:articleRows} = await client.query(
         `
@@ -15,14 +15,17 @@ const getArticleById = async (client, articleId) => {
     var updated_hour = (new Date(updated).getHours() + 9) % 24;
     
     const curhour = new Date().getHours()
-
+    
+    
     if (curhour < updated_hour) {
         updated_hour = null; 
     } else {
         updated_hour = curhour - updated_hour;
     }
-
+    
     articleRows[0].hour = updated_hour;
+    articleRows[0].created_at = dayjs(articleRows[0].created_at).format('MMM DD.YYYY')
+    articleRows[0].updated_at = dayjs(articleRows[0].created_at).format('MMM DD.YYYY')
 
     const { rows:userRows } = await client.query(
         `
@@ -33,11 +36,35 @@ const getArticleById = async (client, articleId) => {
         [articleRows[0].writer]
     );
     
-    const name = userRows[0].name
+    const {rows:comments} = await client.query(
+        `
+        SELECT * FROM "comment" c
+        WHERE article_id = $1
+        `,
+        [articleId]
+    )
 
+    
+
+    for (var i = 0; i < comments.length; i++) {
+        const {rows:commentedUserRows} = await client.query(
+            `
+            SELECT id, name FROM "user"
+            WHERE id = $1
+            `,
+            [comments[i]['user_id']]
+        )
+        
+        comments[i].user = convertSnakeToCamel.keysToCamel(commentedUserRows)
+    }
+    const name = userRows[0].name
+    
     articleRows[0].writer = name
     
-    return convertSnakeToCamel.keysToCamel(articleRows[0])
+    const commentNumber = comments.length
+    const rows = {...articleRows[0], commentNumber, comments:convertSnakeToCamel.keysToCamel(comments)}
+
+    return convertSnakeToCamel.keysToCamel(rows)
 }
 
 module.exports = { getArticleById, }
