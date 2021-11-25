8793f7496d1c991989b7b247cfff441d4a3e07fe
yoon <nicealice99@gmail.com>
2021-11-25 14:20:15 +0900

    [feat]: article list에 commentNumber 추가

--- functions/db/article.js
+++ functions/db/article.js
@@ -2,11 +2,25 @@ const _ = require('lodash');
 const convertSnakeToCamel = require('../lib/converSnakeToCamel');
 
 const getArticleList = async (client) => {
+  let newRows = [];
   const { rows } = await client.query(`
     SELECT * FROM "article" a
     WHERE is_deleted = FALSE
   `);
-  return convertSnakeToCamel.keysToCamel(rows);
+
+  for (let r of rows) {
+    const { rowCount } = await client.query(
+      `
+      SELECT * FROM "comment" c
+      WHERE article_id = $1
+      `,
+      [r.id],
+    );
+    r = { ...r, comment_number: rowCount };
+    newRows.push(r);
+  }
+
+  return convertSnakeToCamel.keysToCamel(newRows);
 };
 
 module.exports = { getArticleList };
